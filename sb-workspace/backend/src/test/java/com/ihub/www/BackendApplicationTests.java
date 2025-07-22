9725caa160a573e9042a5ab626cb31fe818cdcf9
Laxman Konukati <kl752@Laxman.com>
2025-07-22 21:30:20 +0530

    sb project

---
+++ sb-workspace/backend/src/test/java/com/ihub/www/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ihub.www;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
